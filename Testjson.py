fe0146bb327eecd103909ffc79b7e2fc668405c7
Giamma <gianmarialunghi@hotmail.com>
2020-04-13 17:28:45 +0200

    Pulizia etnica

--- Testjson.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('Progettofintech/NASDAQ.json') as json_file:
-    data = json.load(json_file)
-    for p in data:
-        print('Name: ' + p['Company Name'])
-        print('Codice: ' + p['Symbol'])
